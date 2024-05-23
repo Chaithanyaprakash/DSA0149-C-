d7eb91c384b9445106f852ef5d8b9770e49de47d
Chaithanyaprakash <129160651+Chaithanyaprakash@users.noreply.github.com>
2024-05-23 12:04:13 +0530

    Add files via upload

---
+++ print N natural numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i;
+    cout<<"enter limit: ";
+    cin>>n;
+    cout<<"natural numbers are ";
+    for(i=1;i<=n;i++)
+    {
+        cout<<i<<",";
+    }
+    return 0;
+}
