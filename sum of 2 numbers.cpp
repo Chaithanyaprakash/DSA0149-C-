10e2714c12ba93019e48418ecfbc8abc859c8b2a
Chaithanyaprakash <129160651+Chaithanyaprakash@users.noreply.github.com>
2024-05-23 12:01:51 +0530

    Add files via upload

---
+++ sum of 2 numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,sum=0;
+    cout<<"enter num1: ";
+    cin>>a;
+    cout<<"enter num2: ";
+    cin>>b;
+    sum+=a+b;
+    cout<<"sum= "<<sum<<endl;
+    return 0;
+}
