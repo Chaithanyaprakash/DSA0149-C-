0ce613491bbe378b3ccbd929a416dcac685ebc19
Chaithanyaprakash <129160651+Chaithanyaprakash@users.noreply.github.com>
2024-05-28 10:32:42 +0530

    Add files via upload

---
+++ average of 3 numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,avg;
+    cout<<"enter num1: ";
+    cin>>a;
+    cout<<"enter num2: ";
+    cin>>b;
+    cout<<"enter num3: ";
+    cin>>c;
+    avg=(a+b+c)/3;
+    cout<<"average = "<<avg;
+    return 0;
+}
